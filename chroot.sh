f2626a31b85d5098f96933dd05c0fead47bda4fc
Doraemon-kun <doraemon-kun@protonmail.com>
2021-08-18 10:42:07 +0700

    Update more information about installing packages

--- chroot.sh
+++ chroot.sh
@@ -1,4 +1,7 @@
 # Add your local timezone to localtime config and set the timezone to the system
+echo "------------------------------------------------"
+echo "Setting time and timezone..."
+echo "------------------------------------------------"
 ln -sf /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
 timedatectl set-timezone Asia/Ho_Chi_Minh
 
@@ -9,6 +12,9 @@ timedatectl set-ntp 1
 hwclock --systohc
 
 # Localization with locale and keymap
+echo "------------------------------------------------"
+echo "Localizating..."
+echo "------------------------------------------------"
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 localectl set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
@@ -16,11 +22,17 @@ localectl set-keymap us
 
 # Ask for hostname and apply it
 read -p "Hey! Enter hostname please? " hostname
+echo "------------------------------------------------"
+echo "Applying hostname and hosts file"
+echo "------------------------------------------------"
 echo "$hostname" >> /etc/hostname
 
 # Apply hosts file
 echo -e "127.0.0.1     localhost\n::1           localhost\n127.0.1.1     $hostname.localdomain $hostname" >> /etc/hosts
 
+echo "------------------------------------------------"
+echo "Install boot image and configuring GRUB..."
+echo "------------------------------------------------"
 # Set configurations for boot image and apply it
 sed -i 's/^HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev autodetect keyboard keymap consolefont modconf block encrypt filesystems fsck)/' /etc/mkinitcpio.conf
 mkinitcpio -P
@@ -30,9 +42,15 @@ grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
 cp grub /etc/default/
 grub-mkconfig -o /boot/grub/grub.cfg
 
+echo "------------------------------------------------"
+echo "Configuring network... (Please ignore the errors about bus)"
+echo "------------------------------------------------"
 # Enable NetworkManager (ignore the error)
 systemctl enable --now NetworkManager
 
+echo "------------------------------------------------"
+echo "Configuring users and sudoers"
+echo "------------------------------------------------"
 # Config sudoers
 sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 
@@ -44,18 +62,28 @@ useradd --create-home $username
 passwd $username
 usermod -aG wheel $username
 
+echo "------------------------------------------------"
+echo "Configuring pacman... (Maybe takes about five minutes)"
+echo "------------------------------------------------"
 # Configure pacman
 curl -s "https://archlinux.org/mirrorlist/?country=HK&country=ID&country=JP&country=SG&country=TW&country=TH&country=US&protocol=https&ip_version=4&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 15 - > /etc/pacman.d/mirrorlist
 nc=$(grep -c ^processor /proc/cpuinfo)
 sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$nc"/g' /etc/makepkg.conf
 sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g' /etc/makepkg.conf
 
+echo "------------------------------------------------"
+echo "Installing necessary packages for Xorg and some others necesscities..."
+echo "------------------------------------------------"
 # Install more necessary packages
 sudo pacman -Sy --noconfirm xorg-server xorg-xinit xorg-xinput xorg-drivers picom openvpn pulseaudio pavucontrol bash-completion cronie gufw htop neofetch numlockx openssh p7zip unrar unzip wget zsh zsh-completions btrfs-progs dosfstools exfat-utils gparted ntfs-3g parted nautilus gedit clang cmake python xdg-user-dirs
 
+echo "------------------------------------------------"
+echo "Finalizing..."
+echo "------------------------------------------------"
 # Finalizing
 export HOME="/home/$username"
 xdg-user-dirs-update
+export HOME="/root"
 
 # Exit out
 exit
